07779108f078a991c6bd01718dc02b59f713df9d
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 19:18:31 -0300

    test: verify email adress when creating a user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,8 +1,11 @@
+import { left } from "../shared/Either"
+import { InvalidEmailError } from "./errors/invalid-email-error"
 import User from "./user"
 
 describe("User domain entity", () => {
 	test("should not create user with invalid email address", () => {
-		// const invalid_email = "invalid_email"
-		// const error = User.create({ name: "any_name", email: invalid_email})
+		const invalid_email = "invalid_email"
+		const error = User.create({ name: "any_name", email: invalid_email})
+		expect(error).toEqual(left(new InvalidEmailError()))
 	})
 })
