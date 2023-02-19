67391ab1c1106c2344c3a49c11c6222729d31a87
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 18:10:33 -0300

    test: validade email larger than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,9 +16,13 @@ describe("Email validation", () => {
 		expect(Email.validate(email)).toBeTruthy()
 	})
 
-    
 	test("should not accept local part larger than 64 chars", () => {
 		const email = "l".repeat(65) + "@gamil.com"
+		expect(Email.validate(email)).toBeFalsy()
+	})
+
+	test("should not accept string larger than 320 chars", () => {
+		const email = "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127)
 		expect(Email.validate(email)).toBeFalsy()
 	})
 })
