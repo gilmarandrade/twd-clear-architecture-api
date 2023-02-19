65d9936871589f756341c692bf916dd629d167d2
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 18:55:54 -0300

    refactor: move user data to entities layer

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,8 @@
+import User from "./user"
+
+describe("User domain entity", () => {
+	test("should not create user with invalid email address", () => {
+		// const invalid_email = "invalid_email"
+		// const error = User.create({ name: "any_name", email: invalid_email})
+	})
+})
