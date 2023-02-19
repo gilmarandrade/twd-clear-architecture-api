0a6a2d9b9b836916f863086d5b3b226be62fa025
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 11:01:45 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	roots: ["/src"],
+	clearMocks: true,
+	coverageDirectory: "coverage",
+	coverageProvider: "v8",
+	collectCoverageFrom: ["/src/**/*.ts"],
+	testEnvironment: "node",
+	transform: {
+		".+\\.ts$": "ts-jest",
+	},
+}
