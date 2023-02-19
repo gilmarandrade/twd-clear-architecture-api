178384aaaea9256e0ba2ed34053ea40a66088eb5
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 19:18:08 -0300

    feat: verify email adress when creating a user

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,33 +1,51 @@
+import { Either, left, right } from "../shared/Either"
+import { InvalidEmailError } from "./errors/invalid-email-error"
+
 export class Email {
-	static validate(email: string): boolean {
-		const emailRegex =
+
+    private readonly email: string
+
+    constructor(email: string) {
+    	this.email = email
+    }
+
+    static create(email: string): Either<InvalidEmailError, Email> {
+    	if(Email.validate(email)) {
+    		return right(new Email(email))
+    	}
+
+    	return left(new InvalidEmailError())
+    }
+
+    static validate(email: string): boolean {
+    	const emailRegex =
         /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
         
-		if(!email) {
-			return false
-		} 
+    	if(!email) {
+    		return false
+    	} 
 
-		if(email.length > 320) {
-			return false
-		}
+    	if(email.length > 320) {
+    		return false
+    	}
 
-		if(!emailRegex.test(email)) {
-			return false
-		}
+    	if(!emailRegex.test(email)) {
+    		return false
+    	}
 
-		const [local, domain] = email.split("@")
-		if(local.length > 64 || local.length == 0) {
-			return false
-		}
-		if(domain.length > 255|| domain.length == 0) {
-			return false
-		}
+    	const [local, domain] = email.split("@")
+    	if(local.length > 64 || local.length == 0) {
+    		return false
+    	}
+    	if(domain.length > 255|| domain.length == 0) {
+    		return false
+    	}
 
-		const domainParts = domain.split(".")
-		if(domainParts.some(part => part.length > 63)) {
-			return false
-		}
-		return true
+    	const domainParts = domain.split(".")
+    	if(domainParts.some(part => part.length > 63)) {
+    		return false
+    	}
+    	return true
 		
-	}
+    }
 }
--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,14 @@
+import { Either, left } from "../shared/Either"
+import { Email } from "./email"
+import { InvalidEmailError } from "./errors/invalid-email-error"
+import { UserData } from "./user-data"
+
 export default class User {
-    
+	static create(userData: UserData): Either<InvalidEmailError, User> {
+		const emailOrError = Email.create(userData.email)
+
+		if(emailOrError.isLeft()){
+			return left(new InvalidEmailError())
+		}
+	}
 }
