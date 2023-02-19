1a61685f6eb63ea278d1f6d1c69f4038bb60904b
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-19 15:13:34 -0300

    test: find, add and check user

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,13 @@ describe("In memory User repository", () => {
 		const user = await userRepo.findUserByEmail("any@email.com")
 		expect(user).toBeNull()
 	})
+	test("should return user if user is found", async () => {
+		const users: UserData[] = []
+		const userRepo = new InMemoryUserRepository(users)
+		const name = "any_name"
+		const email = "any@email.com"
+		await userRepo.add({name, email})
+		const user = await userRepo.findUserByEmail("any@email.com")
+		expect(user.name).toBe("any_name")
+	})
 })
