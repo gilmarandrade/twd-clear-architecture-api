6c75a2021d588a78e928b9f0c9b48e03a47c059f
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-20 14:17:40 -0300

    test: should not add user with invalid name

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from "../../entities/errors/invalid-email-error"
+import InvalidNameError from "../../entities/errors/invalid-name-error"
 import { UserData } from "../../entities/user-data"
 import { left } from "../../shared/Either"
 import { UserRepository } from "./ports/user-repository"
@@ -32,6 +33,20 @@ describe("Register user on mailing list use case", ()=> {
 		const user = await repo.findUserByEmail(email)
 		expect(user).toBeNull()
 		expect(response).toEqual(left(new InvalidEmailError()))
+	})
+
+	test("should not add user with invalid name", async ()=> {
+		const users: UserData[] = []
+		const repo: UserRepository = new InMemoryUserRepository(users)
+		const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+		const name = ""
+		const email = "any@email.com"
+
+		const response = await usecase.registerUserOnMailingList({ name, email })
+		const user = await repo.findUserByEmail(email)
+		expect(user).toBeNull()
+		expect(response).toEqual(left(new InvalidNameError()))
 	})
 
 })
