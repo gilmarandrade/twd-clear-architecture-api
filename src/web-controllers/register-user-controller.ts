dc63f9ec99150a031da58153c5b2af3957f5f6ab
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-20 17:09:23 -0300

    feat: controller shoud return 400 when request is missing params

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,6 +1,7 @@
 import { UserData } from "@/entities"
 import { RegisterUserOnMailingList } from "@/usecases/register-user-on-mailing-list"
 import { HttpRequest, HttpResponse } from "./ports"
+import { MissingParamError } from "./ports/erros"
 import { created, badRequest } from "./util"
 
 export class RegisterUserController {
@@ -11,6 +12,19 @@ export class RegisterUserController {
     }
 
     public async handle(request: HttpRequest): Promise<HttpResponse> {
+
+    	const missingParams = []
+    	if(!request.body.name) {
+    		missingParams.push("name")
+    	}
+    	if(!request.body.email) {
+    		missingParams.push("email")
+    	}
+        
+    	if(missingParams.length) {
+    		return badRequest(new MissingParamError(missingParams.join(", ")))
+    	}
+        
     	const userData: UserData = request.body
     	const response = await this.usecase.registerUserOnMailingList(userData)
 
