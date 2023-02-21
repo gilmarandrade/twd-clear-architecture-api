08293d7a800a1e3707c1616c0884a7e4d4392ec4
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-20 21:46:40 -0300

    feat: register user controller factory

---
+++ src/main/factories/register.ts
@@ -0,0 +1,12 @@
+import { RegisterUserController } from "@/web-controllers"
+import { RegisterUserOnMailingList } from "@/usecases/register-user-on-mailing-list"
+import { UserRepository } from "@/usecases/register-user-on-mailing-list/ports"
+import { InMemoryUserRepository } from "@test/usecases/register-user-on-mailing-list/repository"
+import { UserData } from "@/entities"
+
+export const makeRegisterUserController = (): RegisterUserController => {
+	const userData: UserData[] = []
+	const repo: UserRepository = new InMemoryUserRepository(userData)
+	const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+	return new RegisterUserController(useCase)
+}
