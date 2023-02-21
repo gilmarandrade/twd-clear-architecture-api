842d7d487c79fe861516d0996371266a7d5c548d
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-21 16:15:50 -0300

    test: connect to mongodb

--- test/main/routes/register-route.test.ts
+++ test/main/routes/register-route.test.ts
@@ -1,7 +1,21 @@
 import request from "supertest"
 import app from "@/main/config/app"
+import { MongoHelper } from "@/external/repositories/mongodb/helper"
 
 describe("Register router", () => {
+	beforeAll(async () => {
+		await MongoHelper.connect(process.env.MONGO_URL)
+	})
+
+	afterAll(async () => {
+		await MongoHelper.disconnect()
+	})
+
+	beforeEach(async () => {
+		MongoHelper.clearCollection("users")
+	})
+
+
 	test("should return an account on success", async () => {
 		// app.post("/test_cors", (req, res) => {
 		// 	res.send()
