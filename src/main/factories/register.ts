b3c458bafdf28610fdf2758b1f7897ed04e3aa2a
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-21 15:34:20 -0300

    refactor: se mongodb user repository

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,12 +1,10 @@
 import { RegisterUserController } from "@/web-controllers"
 import { RegisterUserOnMailingList } from "@/usecases/register-user-on-mailing-list"
 import { UserRepository } from "@/usecases/register-user-on-mailing-list/ports"
-import { InMemoryUserRepository } from "@/usecases/register-user-on-mailing-list/repository"
-import { UserData } from "@/entities"
+import { MongodbUserRepository } from "@/external/repositories/mongodb"
 
 export const makeRegisterUserController = (): RegisterUserController => {
-	const userData: UserData[] = []
-	const repo: UserRepository = new InMemoryUserRepository(userData)
+	const repo: UserRepository = new MongodbUserRepository()
 	const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
 	return new RegisterUserController(useCase)
 }
