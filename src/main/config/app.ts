aefd38c73365638c3c1fa2d6d4856d6775aced97
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-20 23:01:57 -0300

    feat: config register route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import setupMiddleware from "@/main/config/setupMiddleware"
+import setupRoute from "@/main/config/routes"
 
 const app = express()
 
 setupMiddleware(app)
+setupRoute(app)
 
 export default app
