607493b30ed44d4292c13d8839b030b6d0f08ec6
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-22 20:25:48 -0300

    chore: use process.env.PORT

--- src/main/server.ts
+++ src/main/server.ts
@@ -7,8 +7,8 @@ MongoHelper.connect(process.env.MONGO_URL)
 	.then(async () => {
 		const app = (await import("./config/app")).default
 
-		app.listen(5000, () => {
-			console.log("Server running at http://localhost:5000")
+		app.listen(process.env.PORT || 5000, () => {
+			console.log("Server running at port " + (process.env.PORT || 5000))
 		})
 	})
 	.catch(console.error)
