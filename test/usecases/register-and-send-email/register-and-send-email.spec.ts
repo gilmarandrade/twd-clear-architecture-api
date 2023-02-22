044f910163b37cd66e64626ce4f9d5aa26e1b83c
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-22 11:32:41 -0300

    test: register and send email

---
+++ test/usecases/register-and-send-email/register-and-send-email.spec.ts
@@ -0,0 +1,66 @@
+import { User, UserData } from "@/entities"
+import { Either, right } from "@/shared"
+import { MailServiceError } from "@/usecases/errors/mail-service-error"
+import { RegisterAndSendEmail } from "@/usecases/register-and-send-email"
+import { RegisterUserOnMailingList } from "@/usecases/register-user-on-mailing-list"
+import { UserRepository } from "@/usecases/register-user-on-mailing-list/ports"
+import { InMemoryUserRepository } from "@/usecases/register-user-on-mailing-list/repository"
+import { SendEmail } from "@/usecases/send-email"
+import { EmailOptions, EmailService } from "@/usecases/send-email/ports"
+
+const attachmentFilePath = "../resources/test.txt"
+const fromName = "Fromname"
+const fromEmail = "from@email.com"
+const toName = "To name"
+const toEmail = "to@email.com"
+const subject = "Testando email"
+const emailBody = "Hello World"
+const emailBodyHtml = "<b>Hello World</b>"
+const attachment = [
+	{
+		filepath: attachmentFilePath,
+		contentType: "text/plain"
+	}
+]
+
+const mailOptions: EmailOptions = {
+	host: "test",
+	port: 867,
+	username: "test",
+	password: "test",
+	from: fromName + " " + fromEmail,
+	to: toName + " <" + toEmail + ">",
+	subject: subject,
+	text: emailBody,
+	html: emailBodyHtml,
+	attachment: attachment
+}
+
+class MailServiceMock implements EmailService {
+    public timesSendWasCalled = 0
+    
+    async send(emailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    	this.timesSendWasCalled++
+    	return right(emailOptions)
+    }
+}
+
+describe("Register user and send email use case", () => {
+	test("should add user with complete data to mailing list and send email", async ()=> {
+		const users: UserData[] = []
+		const repo: UserRepository = new InMemoryUserRepository(users)
+		const registerUsecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+		const mailServiceMock = new MailServiceMock()
+		const sendEmailUseCase: SendEmail = new SendEmail(mailOptions, mailServiceMock)
+
+		const registerAndSendEmailUseCase: RegisterAndSendEmail = new RegisterAndSendEmail(registerUsecase, sendEmailUseCase)
+		const name = "any_name"
+		const email = "any@email.com"
+
+		const response: User = (await registerAndSendEmailUseCase.perform({ name, email })).value as User
+		const user = await repo.findUserByEmail("any@email.com")
+		expect(user.name).toBe("any_name")
+		expect(response.name.value).toBe("any_name")
+		expect(mailServiceMock.timesSendWasCalled).toBe(1)
+	})
+})
